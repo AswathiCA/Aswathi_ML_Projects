c9cf7ce77fd3fe7e136888b00d4d75c8e405246b
AswathiCA <86aswathi@gmail.com>
2024-04-24 11:32:04 +0530

    my commit

---
+++ DecisionEx.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+df=pd.read_csv(r'water_potability.csv')
+df.info()
+df.isnull().sum()
+df['ph'].fillna(df['ph'].mean(), inplace=True)
+df['Sulfate'].fillna(df['Sulfate'].mean(), inplace=True)
+df['Trihalomethanes'].fillna(df['Trihalomethanes'].mean(), inplace=True)
+x = df.drop(columns=['Potability'])
+y = df['Potability']
+X = df.iloc[:, :-1]
+Y = df.iloc[:, -1]
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
+from sklearn.preprocessing import StandardScaler
+st_x= StandardScaler()
+x_train= st_x.fit_transform(x_train)
+x_test= st_x.transform(x_test)
+from sklearn.tree import DecisionTreeClassifier
+classifier= DecisionTreeClassifier(criterion='entropy', random_state=0)
+classifier.fit(x_train, y_train)
+y_pred= classifier.predict(x_test)
+from sklearn.metrics import confusion_matrix
+cm= confusion_matrix(y_test, y_pred)
+cm
+accuracy = accuracy_score(y_test, y_pred)
+print('Accuracy: %.2f' % (accuracy*100))
